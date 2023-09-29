67e9d311e8e60a64dbef3c584267af7ef4540bf5
piupiupiuboom <throughthearbor@163.com>
2023-09-29 22:26:54 +0800

    add git sign

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,5 +1,22 @@
 return {
-    event = "VeryLazy",
-    "tpope/vim-fugitive",
-        cmd = "Git",
+    {
+        event = "VeryLazy",
+        "tpope/vim-fugitive",
+            cmd = "Git",
+        config = function ()
+            vim.cmd.cnoreabbrev([[git Git]])
+            vim.cmd.cnoreabbrev([[gp Git push]])
+        end
+    },
+    {
+        event = "VeryLazy",
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+        end
+    },
+    {
+        event = "VeryLazy",
+        "rhysd/conflict-marker.vim",
+    },
 }
